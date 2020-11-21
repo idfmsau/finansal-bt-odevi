16cb89362bc026b6721044dfa142bf2a1ea7fad3
Hilal İlayda Tezgider <ilayda.tezgider@hotmail.com>
2020-11-21 21:13:50 +0300

    Create MovieListScreen.js
    Movie List Page added.

---
+++ screens/MovieListScreen.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+class MovieListScreen extends Component {
+    render() {
+        return (
+            <View>
+                <Text> Movie List Page </Text>
+            </View>
+        )
+    }
+}
+
+export default MovieListScreen;
