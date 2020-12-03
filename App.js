ec19cb3a826a7a71907b28639c2126e28e4a0f10
dilan16 <74680666+dilan16@users.noreply.github.com>
2020-12-03 15:56:36 +0200

    test 2

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ export default function App() {
         <>
           <AuthenticatedNavigation />
         </>
-      ) : (
+      ) : ( 
         <NonAuthenticatedNavigation/>
       )}
     </ApplicationProvider>
