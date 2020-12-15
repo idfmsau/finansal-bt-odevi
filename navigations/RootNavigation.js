c615412cee0534b991560e9d35936ebd715df73c
idfmsau <idfmsau@gmail.com>
2020-12-15 19:37:43 +0300

    Auth-nonauth boolean control fixed.

---
+++ navigations/RootNavigation.js
@@ -0,0 +1,29 @@
+import React from "react";
+import AuthenticatedNavigation from "../navigations/AuthenticatedNavigation";
+import NonAuthenticatedNavigation from "../navigations/NonAuthenticatedNavigation";
+import { NavigationContainer } from "@react-navigation/native";
+import { connect } from "react-redux";
+
+
+// import { Provider } from "react-redux";
+const RootNavigation = ({loggedUser}) => {
+    return (
+        <NavigationContainer>
+        {loggedUser.isLoggedIn ? (
+          <>
+            <AuthenticatedNavigation />
+          </>
+        ) : (
+          <NonAuthenticatedNavigation />
+        )}
+      </NavigationContainer>
+    )
+}
+
+const mapStateToProps = (state) => {
+    return {
+      loggedUser: state.auth.loggedUser,
+    };
+  };
+
+export default connect(mapStateToProps)(RootNavigation);
